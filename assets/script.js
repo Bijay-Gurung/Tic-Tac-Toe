3aaa9e8eddd867869e4d9800d06d64ae60222296
Bijay-Gurung <stha4580@gmail.com>
2023-10-31 11:27:41 +0545

    completing home page

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,25 @@
-ECHO is on.
+function tabs(){
+    const tablinks = document.querySelectorAll('header nav a');
+    const tabcontent = document.querySelectorAll('section .main');
+
+    tablinks.forEach(tablink =>{
+        tablink.addEventListener('click',(event) => {
+            event.preventDefault();
+
+            tablinks.forEach(link =>{
+                link.classList.remove('active');
+            });
+
+            tablink.classList.add('active');
+
+            tabcontent.forEach(content =>{
+                content.style.display = 'none';
+            });
+
+            const targetID = tablink.getAttribute('data-toggle');
+            const targetcontent = document.getElementById(targetID);
+            targetcontent.style.display = 'block';
+        });
+    });
+}
+tabs();
