43a1f9166e8b032de63fd8569e574d5bec9ae37d
Bijay-Gurung <stha4580@gmail.com>
2023-11-03 19:16:12 +0545

    deleting unnecessary file

--- assets/script.js
+++ assets/script.js
@@ -23,3 +23,33 @@ function tabs(){
     });
 }
 tabs();
+
+function help(){
+    const helpbtn = document.getElementById('help');
+    const modal = document.getElementById('helps');
+    const closebtn = document.getElementById('close');
+    
+    helpbtn.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
+    
+    closebtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+}
+help();
+
+function credit(){
+    const creditbtn = document.getElementById('credit');
+    const modals = document.getElementById('credits');
+    const closedbtn = document.getElementById('close1');
+
+    creditbtn.addEventListener('click',() =>{
+        modals.style.display = 'block';
+    });
+
+    closedbtn.addEventListener('click', () =>{
+        modals.style.display = 'none';
+    });
+}
+credit();
--- assets/setting.js
+++ assets/setting.js
@@ -1,33 +1,3 @@
-function help(){
-    const helpbtn = document.getElementById('help');
-    const modal = document.getElementById('helps');
-    const closebtn = document.getElementById('close');
-    
-    helpbtn.addEventListener('click', () => {
-        modal.style.display = 'block';
-    });
-    
-    closebtn.addEventListener('click', () => {
-        modal.style.display = 'none';
-    });
-}
-help();
-
-function credit(){
-    const creditbtn = document.getElementById('credit');
-    const modals = document.getElementById('credits');
-    const closedbtn = document.getElementById('close1');
-
-    creditbtn.addEventListener('click',() =>{
-        modals.style.display = 'block';
-    });
-
-    closedbtn.addEventListener('click', () =>{
-        modals.style.display = 'none';
-    });
-}
-credit();
-
 function music(){
     const musicRadio = document.getElementById('musicRadio');
 const sfxRadio = document.getElementById('sfxRadio');
