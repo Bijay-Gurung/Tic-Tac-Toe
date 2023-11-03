2744673d5b98a6662afb4a11df6cb122cfe08f14
Bijay-Gurung <stha4580@gmail.com>
2023-11-03 15:08:41 +0545

    modification

--- assets/setting.js
+++ assets/setting.js
@@ -27,3 +27,29 @@ function credit(){
     });
 }
 credit();
+
+function music(){
+    const musicRadio = document.getElementById('musicRadio');
+const sfxRadio = document.getElementById('sfxRadio');
+const music = document.getElementById('music');
+const sfx = document.getElementById('sfx');
+
+musicRadio.addEventListener('change', function(){
+    if(musicRadio.checked){
+        music.play();
+    }
+    else{
+        music.pause();
+    }
+});
+
+sfxRadio.addEventListener('change', function(){
+    if(sfxRadio.checked){
+        sfx.play();
+    }
+    else{
+        sfx.pause();
+    }
+});
+}
+music();
