6b8b80cd8a998da314fd72394b1553a994fed907
Bijay-Gurung <stha4580@gmail.com>
2023-11-05 15:43:50 +0545

    creating single player mode

--- assets/game.js
+++ assets/game.js
@@ -9,49 +9,68 @@ const tile8 = document.getElementById("div8");
 const tile9 = document.getElementById("div9");
 const result = document.getElementById("result");
 const board = [tile1,tile2,tile3,tile4,tile5,tile6,tile7,tile8,tile9];
+let moves = 0;
 
-/*initializing the game board*/ 
-function initializeGameBoard(){
-    for(let i=0; i<board.length;i++){
+function initializeGameBoard() {
+    for (let i = 0; i < board.length; i++) {
         board[i].innerHTML = " ";
     }
+    moves = 0;
+    result.innerHTML = "";
 }
-initializeGameBoard();
 
-/*defining player Move*/
-function playerMove(){
-    for(let i=0; i<board.length; i++){
-        board[i].addEventListener('click',(event) => {
-            const player = "X";
-            const winText = "You Win";
-            if(board[i].innerHTML === " "){
-                board[i].innerHTML = player;
-                if(board[0].innerHTML==="X" && board[1].innerHTML==="X" && board[2].innerHTML==="X"){
-                    result.innerHTML = winText;
-                }
-                if(board[3].innerHTML==="X" && board[4].innerHTML==="X" && board[5].innerHTML==="X"){
-                    result.innerHTML = winText;
-                }
-                if(board[6].innerHTML==="X" && board[7].innerHTML==="X" && board[8].innerHTML==="X"){
-                    result.innerHTML = winText;
-                }
-                if(board[0].innerHTML==="X" && board[3].innerHTML==="X" && board[6].innerHTML==="X"){
-                    result.innerHTML = winText;
-                }
-                if(board[1].innerHTML==="X" && board[4].innerHTML==="X" && board[7].innerHTML==="X"){
-                    result.innerHTML = winText;
-                }
-                if(board[2].innerHTML==="X" && board[5].innerHTML==="X" && board[8].innerHTML==="X"){
-                    result.innerHTML = winText;
-                }
-                if(board[0].innerHTML==="X" && board[4].innerHTML==="X" && board[8].innerHTML==="X"){
-                    result.innerHTML = winText;
-                }
-                if(board[6].innerHTML==="X" && board[4].innerHTML==="X" && board[2].innerHTML==="X"){
-                    result.innerHTML = winText;
-                }
-            }
-        });
+function playerMove(i) {
+    const player = "X";
+    if (board[i].innerHTML === " " && result.innerHTML === "") {
+        board[i].innerHTML = player;
+        moves++;
+        checkResult(player);
+        computerMove();
     }
 }
-playerMove();
+
+function computerMove() {
+    const computer = "O";
+    if (result.innerHTML === "") {
+        const emptyTiles = [];
+        for (let i = 0; i < board.length; i++) {
+            if (board[i].innerHTML === " ") {
+                emptyTiles.push(i);
+            }
+        }
+        if (emptyTiles.length > 0) {
+            const randomIndex = Math.floor(Math.random() * emptyTiles.length);
+            const chosenTileIndex = emptyTiles[randomIndex];
+            board[chosenTileIndex].innerHTML = computer;
+            moves++;
+            checkResult(computer);
+        }
+    }
+}
+
+function checkResult(player) {
+    const winText = `${player} Wins!`;
+    if (
+        (board[0].innerHTML === player && board[1].innerHTML === player && board[2].innerHTML === player) ||
+        (board[3].innerHTML === player && board[4].innerHTML === player && board[5].innerHTML === player) ||
+        (board[6].innerHTML === player && board[7].innerHTML === player && board[8].innerHTML === player) ||
+        (board[0].innerHTML === player && board[3].innerHTML === player && board[6].innerHTML === player) ||
+        (board[1].innerHTML === player && board[4].innerHTML === player && board[7].innerHTML === player) ||
+        (board[2].innerHTML === player && board[5].innerHTML === player && board[8].innerHTML === player) ||
+        (board[0].innerHTML === player && board[4].innerHTML === player && board[8].innerHTML === player) ||
+        (board[2].innerHTML === player && board[4].innerHTML === player && board[6].innerHTML === player)
+    ) {
+        result.innerHTML = winText;
+    } else if (moves === 9) {
+        result.innerHTML = "It's a Draw!";
+    }
+}
+
+function attachClickListeners() {
+    for (let i = 0; i < board.length; i++) {
+        board[i].addEventListener('click', () => playerMove(i));
+    }
+}
+
+initializeGameBoard();
+attachClickListeners();
